2cae0c91d38591bd3fa9c4c4348b5ac1d3cb0924
linbets <lindseybetsinger@gmail.com>
2022-04-01 18:23:44 -0500

    additional functionality

--- script.js
+++ script.js
@@ -3,9 +3,41 @@
 //Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //Starting Conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Rolling Dice Functionality
+btnRoll.addEventListener('click', function () {
+  //1. Random Number Generation
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //2. Display Dice Function
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. Check if dice is 1
+  if (dice !== 1) {
+    //add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+  }
+});
